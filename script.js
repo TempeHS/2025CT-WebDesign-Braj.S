8a72dccfb8b9f3c154893dbc21f7686aeba7e6f5
Braj Sapkota <braj.sapkota@DETNSW>
2024-03-11 14:13:39 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let myIntro = 15;
-let enterName = 15;
+let enterName = prompt("Enter your name");
+let PlayerGuess;
+let PlayerFeedback = "test";
+let computerGuess;
+loadDIV ();
 
-if (myIntro == enterName) {
-    alert(myIntro);
+
+
+function loadDIV () {
+document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>"
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "</H1><H2>Guess a number between 1 & 2</H2>"
+document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + PlayerFeedback
+}
+
+function enterNumber () {
+    PlayerGuess = prompt("Enter a number")
 }
