2d25072cdc4c5d7f49d0933aa01a79bfd94723ea
Braj Sapkota <braj.sapkota@DETNSW>
2024-03-13 15:04:16 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let enterName = prompt("Enter your name");
-let PlayerGuess;
-let PlayerFeedback = "test";
-let computerGuess;
-loadDIV ();
+let playerGuess;
+//let computerGuess = randomNum();
+let playerFeedback = enterName;
+loadDIV();
 
+function randomNum() {  
+  return Math.floor(Math.random() * (10 - 1 + 1) + 1)
+}
 
 function loadDIV () {
-document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>"
-document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "</H1><H2>Guess a number between 1 & 2</H2>"
-document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + PlayerFeedback
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess a number between 1 & 10</H2>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
 }
 
 function enterNumber () {
-    PlayerGuess = prompt("Enter a number")
+    playerGuess = prompt("Enter a number") 
 }
